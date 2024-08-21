60e1b3a03aa24a01a643d2ea9ad0e9d8ea5129f1
yotsubo <513047.otsubo@gmail.com>
2024-08-21 18:33:31 +0900

    norm: get_win_size.c

---
+++ src/get_win_size_utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_win_size_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 18:21:25 by yuotsubo          #+#    #+#             */
+/*   Updated: 2024/08/21 18:22:23 by yuotsubo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+#include "libft.h"
+
+void	adj_point_y(t_data *data, int min)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < data->map_height)
+	{
+		j = 0;
+		while (j < data->map_width)
+		{
+			data->map[i][j]->y += -min;
+			j++;
+		}
+		i++;
+	}
+}
+
+void	adj_point_x(t_data *data, int min)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < data->map_height)
+	{
+		j = 0;
+		while (j < data->map_width)
+		{
+			data->map[i][j]->x += -min;
+			j++;
+		}
+		i++;
+	}
+}
