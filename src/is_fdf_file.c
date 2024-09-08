c23450c4b75470aa79897f508b5bd383e6cb76f5
yotsubo <513047.otsubo@gmail.com>
2024-09-08 15:54:45 +0900

    modified: reject only .fdf

--- src/is_fdf_file.c
+++ src/is_fdf_file.c
@@ -6,19 +6,43 @@
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 15:28:27 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/09/08 15:36:21 by yuotsubo         ###   ########.fr       */
+/*   Updated: 2024/09/08 15:51:38 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "libft.h"
 
-t_bool  is_fdf_file(char *filename)
+static char	*get_file_name(char *file_path)
 {
-    size_t  tail;
+	char	*res;
+	char	*name_head;
 
-    tail = ft_strlen(filename);
-    if (tail < 5)
-        return (FALSE);
-    return (!ft_strncmp(&filename[tail - 4], ".fdf", 4));
+	name_head = ft_strrchr(file_path, '/');
+	if (!name_head)
+		name_head = file_path;
+	else
+		name_head++;
+	res = ft_strdup(name_head);
+	if (!res)
+		return (NULL);
+	return (res);
+}
+
+t_bool	is_fdf_file(char *file_path)
+{
+	char	*filename;
+	size_t	len;
+	t_bool	res;
+
+	filename = get_file_name(file_path);
+	if (!filename)
+		return (FALSE);
+	len = ft_strlen(filename);
+	if (len > 4 && !ft_strncmp(&filename[len - 4], ".fdf", 4))
+		res = TRUE;
+	else
+		res = FALSE;
+	free(filename);
+	return (res);
 }
